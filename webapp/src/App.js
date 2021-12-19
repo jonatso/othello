bb27753c85be709a70c30edd88de0945645591e0
Jonatan Solheim <tilingen@gmail.com>
2021-12-19 18:14:13 +0100

    algorithm to check move legality

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -27,12 +27,14 @@ gameBoard[4][3] = {
   pieceColor: "black"
 }
 
+const directions = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
 function App() {
   const [board, setBoard] = React.useState(gameBoard)
   const [isPlayer1Turn, setIsPlayer1Turn] = React.useState(true)
 
   function placePiece(x, y) {
-    if(board[x][y].hasPiece) return
+    if (!canPlacePiece(x, y, isPlayer1Turn)) return
     setBoard(oldBoard => {
       const newBoard = _.cloneDeep(oldBoard)
       newBoard[x][y].hasPiece = true
@@ -42,8 +44,26 @@ function App() {
     setIsPlayer1Turn(!isPlayer1Turn)
   }
 
-  function canPlacePiece(isPlayer1, x, y) {
+  function canPlacePiece(x, y, isPlayer1Turn) {
+    if (board[x][y].hasPiece) return false
+    if (x > 7 || y > 7 || x < 0 || y < 0) return false
 
+    for (var [dx, dy] of directions) {
+      console.log(dx, dy)
+      if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) continue
+      if (!board[x + dx][y + dy].hasPiece) continue
+      if (board[x + dx][y + dy].pieceColor === (isPlayer1Turn ? "white" : "black")) continue
+      console.log("goingin")
+      if (traverseBoard(x + dx, y + dy, dx, dy, isPlayer1Turn)) return true
+    }
+    return false
+  }
+
+  function traverseBoard(x, y, dx, dy, isPlayer1Turn) {
+    if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) return false
+    if (!board[x + dx][y + dy].hasPiece) return false
+    if (board[x + dx][y + dy].pieceColor === (isPlayer1Turn ? "white" : "black")) return true
+    return traverseBoard(x + dx, y + dy, dx, dy, isPlayer1Turn)
   }
 
   return (
