5c8f7e421e76da8c65396f3048a9e24de1fcf538
Jonatan Solheim <tilingen@gmail.com>
2021-12-20 22:06:56 +0100

    clean up webapp for server connection

--- server/index.js
+++ server/index.js
@@ -8,205 +8,205 @@ const server = http.createServer(app);
 var _ = require("lodash");
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  },
 });
 
 const socketToRoom = {};
 const gameStates = {};
 
 io.on("connection", (socket) => {
-    console.log(`Connected: ${socket.id}`);
+  console.log(`Connected: ${socket.id}`);
 
-    socket.on("disconnect", handleDisconnect);
-    socket.on("joinGame", handleJoinGame);
-    socket.on("createGame", handleCreateGame);
-    socket.on("makeMove", handleMakeMove);
+  socket.on("disconnect", handleDisconnect);
+  socket.on("joinGame", handleJoinGame);
+  socket.on("createGame", handleCreateGame);
+  socket.on("makeMove", handleMakeMove);
 
-    function handleJoinGame(roomName) {
-        const room = io.sockets.adapter.rooms[roomName];
+  function handleJoinGame(roomName) {
+    const room = io.sockets.adapter.rooms[roomName];
 
-        let allUsers;
-        if (room) {
-            allUsers = room.sockets;
-        }
-
-        let numClients = 0;
-        if (allUsers) {
-            numClients = Object.keys(allUsers).length;
-        }
-
-        if (numClients === 0) {
-            socket.emit("badCode", "No players in room");
-            return;
-        } else if (numClients > 1) {
-            socket.emit("badCode", "Room is full");
-            return;
-        }
-
-        socketToRoom[socket.id] = roomName;
-
-        socket.join(roomName);
-        socket.isPlayer1 = false;
-        socket.emit("initGame", false);
-
-        startGameInterval(roomName);
+    let allUsers;
+    if (room) {
+      allUsers = room.sockets;
     }
 
-    function handleCreateGame() {
-        let roomName = makeid(5);
-        socketToRoom[socket.id] = roomName;
-        socket.emit("gameCode", roomName);
-
-        gameStates[roomName] = createGameState();
-
-        socket.join(roomName);
-        socket.isPlayer1 = true;
-        socket.emit("initGame", true);
+    let numClients = 0;
+    if (allUsers) {
+      numClients = Object.keys(allUsers).length;
     }
 
-    function handleDisconnect() {
-        const roomName = socketToRoom[socket.id];
-        delete socketToRoom[socket.id];
-        if (roomName) {
-            const room = io.sockets.adapter.rooms[roomName];
-            if (room) {
-                for (socket of room.sockets) {
-                    socket.emit("opponentLeft");
-                }
-            }
-            delete gameStates[roomName];
-        }
+    if (numClients === 0) {
+      socket.emit("badCode", "No players in room");
+      return;
+    } else if (numClients > 1) {
+      socket.emit("badCode", "Room is full");
+      return;
     }
 
-    function handleMakeMove(data) {
-        const roomName = socketToRoom[socket.id];
-        const gameState = gameStates[roomName];
-        if (gameState.isWhitesTurn !== socket.isPlayer1) {
-            socket.emit("moveError", "Not your turn");
-            return;
+    socketToRoom[socket.id] = roomName;
+
+    socket.join(roomName);
+    socket.isPlayer1 = false;
+    socket.emit("initGame", false);
+
+    startGameInterval(roomName);
+  }
+
+  function handleCreateGame() {
+    let roomName = makeid(5);
+    socketToRoom[socket.id] = roomName;
+    socket.emit("gameCode", roomName);
+
+    gameStates[roomName] = createGameState();
+
+    socket.join(roomName);
+    socket.isPlayer1 = true;
+    socket.emit("initGame", true);
+  }
+
+  function handleDisconnect() {
+    const roomName = socketToRoom[socket.id];
+    delete socketToRoom[socket.id];
+    if (roomName) {
+      const room = io.sockets.adapter.rooms[roomName];
+      if (room) {
+        for (socket of room.sockets) {
+          socket.emit("opponentLeft");
         }
-        if (gameState.possibleMovesBoard[data.x][data.y] === "") {
-            socket.emit("moveError", "Invalid move");
-            return;
-        }
-        if (placePiece(data.x, data.y, gameState)) {
-            io.to(roomName).emit("gameStateUpdate", gameState);
-        }
+      }
+      delete gameStates[roomName];
     }
+  }
+
+  function handleMakeMove(data) {
+    const roomName = socketToRoom[socket.id];
+    const gameState = gameStates[roomName];
+    if (gameState.isWhitesTurn !== socket.isPlayer1) {
+      socket.emit("moveError", "Not your turn");
+      return;
+    }
+    if (gameState.possibleMovesBoard[data.x][data.y] === "") {
+      socket.emit("moveError", "Invalid move");
+      return;
+    }
+    if (placePiece(data.x, data.y, gameState)) {
+      io.to(roomName).emit("gameStateUpdate", gameState);
+    }
+  }
 });
 
 server.listen(3001, () => {
-    console.log("Server is running on port 3001");
+  console.log("Server is running on port 3001");
 });
 
 function makeid(length) {
-    var res = "";
-    const chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charsL = chars.length;
-    for (var i = 0; i < length; i++) {
-        res += chars.charAt(Math.floor(Math.random() * charsL));
-    }
-    return res;
+  var res = "";
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charsL = chars.length;
+  for (var i = 0; i < length; i++) {
+    res += chars.charAt(Math.floor(Math.random() * charsL));
+  }
+  return res;
 }
 
 function createGameState() {
-    return {
-        board: [
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "w", "b", "", "", ""],
-            ["", "", "", "b", "w", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-        ],
-        possibleMovesBoard: [
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "w", "", "", ""],
-            ["", "", "w", "", "", "", "", ""],
-            ["", "", "", "", "", "w", "", ""],
-            ["", "", "", "w", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-        ],
-        isWhitesTurn: true,
-    };
+  return {
+    board: [
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "w", "b", "", "", ""],
+      ["", "", "", "b", "w", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+    ],
+    possibleMovesBoard: [
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "w", "", "", ""],
+      ["", "", "w", "", "", "", "", ""],
+      ["", "", "", "", "", "w", "", ""],
+      ["", "", "", "w", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+    ],
+    isWhitesTurn: true,
+  };
 }
 
 function placePiece(x, y, gameState) {
-    //console.log(`${isWhitesTurn ? 'white' : 'black'} is trying to place a piece`)
-    const flipDirections = getFlipDirections(x, y, gameState);
-    if (flipDirections.length === 0) return false;
-    const newBoard = _.cloneDeep(oldBoard);
-    newBoard[x][y] = gameState.isWhitesTurn ? "w" : "b";
-    for (var [dx, dy] of flipDirections) {
-        flipPieces(x, y, dx, dy, newBoard, gameState);
-    }
+  //console.log(`${isWhitesTurn ? 'white' : 'black'} is trying to place a piece`)
+  const flipDirections = getFlipDirections(x, y, gameState);
+  if (flipDirections.length === 0) return false;
+  const newBoard = _.cloneDeep(oldBoard);
+  newBoard[x][y] = gameState.isWhitesTurn ? "w" : "b";
+  for (var [dx, dy] of flipDirections) {
+    flipPieces(x, y, dx, dy, newBoard, gameState);
+  }
 
-    gameState.board = newBoard;
-    gameState.isWhitesTurn = !gameState.isWhitesTurn;
-    gameState.possibleMovesBoard = getNewPossibleMovesBoard(gameState);
-    return true;
+  gameState.board = newBoard;
+  gameState.isWhitesTurn = !gameState.isWhitesTurn;
+  gameState.possibleMovesBoard = getNewPossibleMovesBoard(gameState);
+  return true;
 }
 
 function getNewPossibleMovesBoard(gameState) {
-    const newPMBoard = Array.from(
+  const newPMBoard = Array.from(
+    {
+      length: 8,
+    },
+    (_) =>
+      Array.from(
         {
-            length: 8,
+          length: 8,
         },
-        (_) =>
-            Array.from(
-                {
-                    length: 8,
-                },
-                (_) => ""
-            )
-    );
+        (_) => ""
+      )
+  );
 
-    for (let x = 0; x < newPMBoard.length; x++) {
-        for (let y = 0; y < newPMBoard[0].length; y++) {
-            if (getFlipDirections(x, y, gameState).length !== 0) {
-                newPMBoard[x][y] = gameState.isWhitesTurn ? "w" : "b";
-            }
-        }
+  for (let x = 0; x < newPMBoard.length; x++) {
+    for (let y = 0; y < newPMBoard[0].length; y++) {
+      if (getFlipDirections(x, y, gameState).length !== 0) {
+        newPMBoard[x][y] = gameState.isWhitesTurn ? "w" : "b";
+      }
     }
+  }
 
-    return newPMBoard;
+  return newPMBoard;
 }
 
 function getFlipDirections(x, y, gameState) {
-    const flipDirections = [];
-    if (gameState.board[x][y] !== "") return flipDirections;
-    if (x > 7 || y > 7 || x < 0 || y < 0) return flipDirections;
+  const flipDirections = [];
+  if (gameState.board[x][y] !== "") return flipDirections;
+  if (x > 7 || y > 7 || x < 0 || y < 0) return flipDirections;
 
-    for (var [dx, dy] of directions) {
-        if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) continue;
-        if (board[x + dx][y + dy] !== (isWhitesTurn ? "b" : "w")) continue;
-        if (searchForMove(x + dx, y + dy, dx, dy, gameState)) {
-            flipDirections.push([dx, dy]);
-        }
+  for (var [dx, dy] of directions) {
+    if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) continue;
+    if (board[x + dx][y + dy] !== (isWhitesTurn ? "b" : "w")) continue;
+    if (searchForMove(x + dx, y + dy, dx, dy, gameState)) {
+      flipDirections.push([dx, dy]);
     }
-    return flipDirections;
+  }
+  return flipDirections;
 }
 
 function searchForMove(x, y, dx, dy, gameState) {
-    if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) return false;
-    if (board[x + dx][y + dy] === (isWhitesTurn ? "w" : "b")) return true;
-    return searchForMove(x + dx, y + dy, dx, dy, gameState);
+  if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) return false;
+  if (board[x + dx][y + dy] === (isWhitesTurn ? "w" : "b")) return true;
+  return searchForMove(x + dx, y + dy, dx, dy, gameState);
 }
 
 function flipPieces(x, y, dx, dy, newBoard, gameState) {
-    while (board[x + dx][y + dy] !== (gameState.isWhitesTurn ? "w" : "b")) {
-        newBoard[x + dx][y + dy] = gameState.isWhitesTurn ? "w" : "b";
-        x += dx;
-        y += dy;
-    }
+  while (board[x + dx][y + dy] !== (gameState.isWhitesTurn ? "w" : "b")) {
+    newBoard[x + dx][y + dy] = gameState.isWhitesTurn ? "w" : "b";
+    x += dx;
+    y += dy;
+  }
 }
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,102 +1,54 @@
-import Board from './components/Board';
-import React from "react"
-import _ from "lodash"
+import Board from "./components/Board";
+import React from "react";
+import _ from "lodash";
 
-const directions = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+const directions = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+];
 
 function App() {
-  const [isWhitesTurn, setisWhitesTurn] = React.useState(true)
+  const [isWhitesTurn, setisWhitesTurn] = React.useState(true);
   const [board, setBoard] = React.useState([
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', 'w', 'b', '', '', ''],
-    ['', '', '', 'b', 'w', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '']
-  ])
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+  ]);
   const [posibleMovesBoard, setPossibleMovesBoard] = React.useState([
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', 'w', '', '', ''],
-    ['', '', 'w', '', '', '', '', ''],
-    ['', '', '', '', '', 'w', '', ''],
-    ['', '', '', 'w', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '']
-  ])
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+  ]);
 
   function placePiece(x, y) {
-    console.log(`${isWhitesTurn ? 'white' : 'black'} is trying to place a piece`)
-    const flipDirections = getFlipDirections(x, y)
-    if (flipDirections.length === 0) return
-    setBoard(oldBoard => {
-      const newBoard = _.cloneDeep(oldBoard)
-      newBoard[x][y] = (isWhitesTurn ? 'w' : 'b')
-      for (var [dx, dy] of flipDirections) {
-        flipPieces(x, y, dx, dy, newBoard)
-      }
-
-      return newBoard
-    })
+    console.log(
+      `${isWhitesTurn ? "white" : "black"} is trying to place a piece`
+    );
   }
-
-  function getNewPossibleMovesBoard() {
-    const newPMBoard = Array.from({length: 8}, _=>(Array.from({length: 8}, _=>(''))))
-
-    for (let x = 0; x < newPMBoard.length; x++) {
-      for (let y = 0; y < newPMBoard[0].length; y++) {
-        if (getFlipDirections(x, y).length !== 0) {
-          newPMBoard[x][y] = (isWhitesTurn ? 'w' : 'b')
-        }
-      }
-    }
-    return newPMBoard
-  }
-
-  function getFlipDirections(x, y) {
-    const flipDirections = []
-    if (board[x][y] !== '') return flipDirections
-    if (x > 7 || y > 7 || x < 0 || y < 0) return flipDirections
-
-    for (var [dx, dy] of directions) {
-      if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) continue
-      if (board[x + dx][y + dy] !== (isWhitesTurn ? 'b' : 'w')) continue
-      if (searchForMove(x + dx, y + dy, dx, dy)) {
-        flipDirections.push([dx, dy])
-      }
-    }
-    return flipDirections
-  }
-
-  function searchForMove(x, y, dx, dy) {
-    if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) return false
-    if (board[x + dx][y + dy] === (isWhitesTurn ? 'w' : 'b')) return true
-    return searchForMove(x + dx, y + dy, dx, dy)
-  }
-
-  function flipPieces(x, y, dx, dy, newBoard) {
-    while (board[x + dx][y + dy] !== (isWhitesTurn ? 'w' : 'b')) {
-      newBoard[x + dx][y + dy] = (isWhitesTurn ? 'w' : 'b')
-      x += dx
-      y += dy
-    }
-  }
-
-  React.useEffect(() => {
-    console.log("changing turns")
-    setisWhitesTurn(!isWhitesTurn)
-  }, [board])
-
-  React.useEffect(() => {
-    console.log("changing possible moves")
-    setPossibleMovesBoard(getNewPossibleMovesBoard())
-  }, [isWhitesTurn])
 
   return (
     <div className="app">
-      <Board board={board} posibleMovesBoard={posibleMovesBoard} handleClick={placePiece}/>
+      <Board
+        board={board}
+        posibleMovesBoard={posibleMovesBoard}
+        handleClick={placePiece}
+      />
     </div>
   );
 }
