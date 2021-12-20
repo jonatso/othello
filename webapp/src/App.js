a121c1d472c1082bb44eea2d42955d4f98ed5c67
Jonatan Solheim <tilingen@gmail.com>
2021-12-20 12:11:19 +0100

    move changes to use effect, works now ey

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,7 +5,7 @@ import _ from "lodash"
 const directions = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 
 function App() {
-  const [isPlayer1Turn, setIsPlayer1Turn] = React.useState(true)
+  const [isWhitesTurn, setisWhitesTurn] = React.useState(true)
   const [board, setBoard] = React.useState([
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
@@ -19,34 +19,31 @@ function App() {
   const [posibleMovesBoard, setPossibleMovesBoard] = React.useState([
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', 'w', '', '', ''],
+    ['', '', 'w', '', '', '', '', ''],
+    ['', '', '', '', '', 'w', '', ''],
+    ['', '', '', 'w', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '']
   ])
 
   function placePiece(x, y) {
+    console.log(`${isWhitesTurn ? 'white' : 'black'} is trying to place a piece`)
     if (!canPlacePiece(x, y)) return
     setBoard(oldBoard => {
       const newBoard = _.cloneDeep(oldBoard)
-      newBoard[x][y] = (isPlayer1Turn ? 'w' : 'b')
+      newBoard[x][y] = (isWhitesTurn ? 'w' : 'b')
       return newBoard
     })
-    setIsPlayer1Turn(!isPlayer1Turn)
-    console.log("getting there")
-    //setPossibleMovesBoard(getNewPossibleMovesBoard())
   }
 
   function getNewPossibleMovesBoard() {
-    console.log("we doing it")
     const newPMBoard = Array.from({length: 8}, _=>(Array.from({length: 8}, _=>(''))))
 
     for (let x = 0; x < newPMBoard.length; x++) {
       for (let y = 0; y < newPMBoard[0].length; y++) {
         if (canPlacePiece(x, y)) {
-          newPMBoard[x][y] = (isPlayer1Turn ? 'w' : 'b')
+          newPMBoard[x][y] = (isWhitesTurn ? 'w' : 'b')
         }
       }
     }
@@ -59,7 +56,7 @@ function App() {
 
     for (var [dx, dy] of directions) {
       if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) continue
-      if (board[x + dx][y + dy] !== (isPlayer1Turn ? 'b' : 'w')) continue
+      if (board[x + dx][y + dy] !== (isWhitesTurn ? 'b' : 'w')) continue
       if (searchForMove(x + dx, y + dy, dx, dy)) return true
     }
     return false
@@ -67,9 +64,19 @@ function App() {
 
   function searchForMove(x, y, dx, dy) {
     if (x + dx > 7 || y + dy > 7 || x + dx < 0 || y + dy < 0) return false
-    if (board[x + dx][y + dy] === (isPlayer1Turn ? 'w' : 'b')) return true
+    if (board[x + dx][y + dy] === (isWhitesTurn ? 'w' : 'b')) return true
     return searchForMove(x + dx, y + dy, dx, dy)
   }
+
+  React.useEffect(() => {
+    console.log("changing turns")
+    setisWhitesTurn(!isWhitesTurn)
+  }, [board])
+
+  React.useEffect(() => {
+    console.log("changing possible moves")
+    setPossibleMovesBoard(getNewPossibleMovesBoard())
+  }, [isWhitesTurn])
 
   return (
     <div className="app">
